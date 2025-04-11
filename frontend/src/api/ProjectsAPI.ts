e657d443a9f3ee25e1cb4a15ff2bbcf55170a1cf
Gavin Smith <gavinjacobsmith@gmail.com>
2025-04-10 22:11:30 -0600

    updated the url

--- frontend/src/api/ProjectsAPI.ts
+++ frontend/src/api/ProjectsAPI.ts
@@ -5,7 +5,7 @@ interface FetchProjectsResponse {
     totalNumProjects: number;
   }
   
-  const API_URL = 'https://waterproject-gavin-v2-e2dkcvdccqhhd7hb.eastus-01.azurewebsites.net/'
+  const API_URL = 'https://waterproject-gavin-v2-e2dkcvdccqhhd7hb.eastus-01.azurewebsites.net/water'
 
   export const fetchProjects = async (
     pageSize: number,
